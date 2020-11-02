dc36066590c700020f6500e390595bd917d048fd
bodroghykristof <bodroghykristof@gmail.com>
2020-11-02 20:20:56 +0100

    Implement generating random matrix

--- src/com/codecool/matrixmultiplication/model/Matrix.java
+++ src/com/codecool/matrixmultiplication/model/Matrix.java
@@ -1,11 +1,22 @@
 package com.codecool.matrixmultiplication.model;
 
+import com.codecool.matrixmultiplication.utility.RandomUtils;
+
 public class Matrix {
 
-    private int[][] matrix;
+    private final int[][] matrix;
 
-    public Matrix(int rows, int columns) {
+    public Matrix(int rows, int columns, int lowerLimitInclusive, int upperLimitInclusive) {
         this.matrix = new int[rows][columns];
+        generateMatrix(lowerLimitInclusive, upperLimitInclusive);
+    }
+
+    private void generateMatrix(int lowerLimitInclusive, int upperLimitInclusive) {
+        for (int[] row : matrix) {
+            for (int i = 0; i < row.length; i++) {
+                row[i] = RandomUtils.randomizeInRange(lowerLimitInclusive, upperLimitInclusive);
+            }
+        }
     }
 
     @Override
